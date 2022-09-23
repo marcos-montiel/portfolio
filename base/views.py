7f96519b888a9aa21dc853965e29b6f3ca279abf
Marcos Montiel <marcosmontielrej@gmail.com>
2022-09-23 15:14:56 -0300

    Work in the send message funtionality

--- base/views.py
+++ base/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.core.mail import send_mail
 
 # Create your views here.
 class IndexView(TemplateView):
   template_name = 'base/index.html'
+
+  def post(self, request, *args, **kwargs):
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    subject = request.POST.get('subject')
+    ms = request.POST.get('message')
+
+    message = name + " - " + ms    
+
+    send_mail(subject, message, email, ['marcosmontielrej@gmail.com'])
